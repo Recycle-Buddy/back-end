9667cb7a5cba14536fcb96311b06c552132a9fd6
starfoe <35882799+starfoe@users.noreply.github.com>
2018-08-07 16:06:09 -0700

    Added base64 encode/decode function

--- src/RecycleBuddyAnnotator.py
+++ src/RecycleBuddyAnnotator.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import numpy as np
 import json
+import base64
+import urllib
 
 import cv2
 from PIL import Image
@@ -11,25 +13,33 @@ from google.cloud import vision
 from google.cloud.vision import types
 
 
-class RecycleBuddyAnnotator:
+class RecycleBuddyAnnotator(object):
     '''
     The class preprocesses an image when needed, recognize texts and objects contained.
     It reads in an image URL and returns a json file with texts, label of objects that have been recognized
     '''
 
     def __init__(self):
-
         self.client = vision.ImageAnnotatorClient()
         print('Google client successfully initiated')
         self.image = vision.types.Image()
 
-    def preprocessingImage(self, imageURL):
-        self.image.source.image_uri = imageURL
+    def base64_encoder(self, image_URL):
+        response = urllib.request.urlopen(image_URL)
+        image_byte = bytearray(response.read())
+        image_64_encode = base64.encodestring(image_byte)
+        return image_64_encode
+
+    def base64_decoder(self, image_64_encode):
+        image_64_decode = base64.decodestring(image_64_encode)
+        return image_64_decode
+
+    def preprocessing_image(self, image_URL):
+        self.image.source.image_uri = image_URL
         pass
 
-
-    def textExtraction(self, imageURL):
-        self.image.source.image_uri = imageURL
+    def text_extraction_URL(self, image_URL):
+        self.image.source.image_uri = image_URL
         response = self.client.text_detection(image=self.image)
         text_info = response.text_annotations
         return_dict = {}
@@ -37,12 +47,10 @@ class RecycleBuddyAnnotator:
         for text in text_info:
             return_dict[text.description] = []
             print('{}\n'.format(text.description))
-
         return_json = json.dumps(return_dict)
         return return_json
 
-
-    def objectExtraction(self, imageURL):
+    def object_extraction_URL(self, imageURL):
         self.image.source.image_uri = imageURL
         response = self.client.label_detection(image=self.image)
         labels_info = response.label_annotations
@@ -51,5 +59,30 @@ class RecycleBuddyAnnotator:
         for label in labels_info:
             return_dict[label.description] = round(label.score,4)
             print('Lable:{} | score:{}\n'.format(label.description,round(label.score,4)))
+        return_json = json.dumps(return_dict)
+        return return_json
+
+    def object_extraction_base64(self,image_64_encode):
+        content = self.base64_decoder(image_64_encode)
+        self.image = vision.types.Image(content=content)
+        response = self.client.label_detection(image=self.image)
+        return_dict = {}
+        print('The image contains the following objects with corresponding score:\n')
+        for label in labels_info:
+            return_dict[label.description] = round(label.score, 4)
+            print('Lable:{} | score:{}\n'.format(label.description, round(label.score, 4)))
+        return_json = json.dumps(return_dict)
+        return return_json
+
+    def text_extraction_base64(self,image_64_encode):
+        content = self.base64_decoder(image_64_encode)
+        self.image = vision.types.Image(content=content)
+        response = self.client.text_detection(image=self.image)
+        text_info = response.text_annotations
+        return_dict = {}
+        print('The image has the following texts:\n')
+        for text in text_info:
+            return_dict[text.description] = []
+            print('{}\n'.format(text.description))
         return_json = json.dumps(return_dict)
         return return_json
--- src/RecycleBuddyMain.py
+++ src/RecycleBuddyMain.py
@@ -1,3 +1,5 @@
+import pickle
+import RecycleBuddyAnnotator as RBA
 import sys
 import datetime
 
@@ -10,19 +12,25 @@ if __name__ == '__main__':
     #     raise ValueError('Please check your parameter')
 
     # imageURL = str(sys.argv[1])
-
-    imageURL = 'https://s3.us-east-2.amazonaws.com/recycle.buddy/IMG_3862.jpg'
+    image_URL = 'https://s3.us-east-2.amazonaws.com/recycle.buddy/IMG_3862.jpg'
     model = RBA.RecycleBuddyAnnotator()
     starttime = datetime.datetime.now()
-    texts_returned = model.textExtraction(imageURL)
+    texts_returned = model.text_extraction(image_URL)
     endtime = datetime.datetime.now()
     interval = endtime-starttime
     print('Text recognition takes {} seconds'.format(interval.seconds))
     starttime = datetime.datetime.now()
-    labels_returned = model.objectExtraction(imageURL)
+    labels_returned = model.object_extraction(image_URL)
     endtime = datetime.datetime.now()
     interval = endtime - starttime
     print('Object recognition takes {} seconds'.format(interval.seconds))
+
+    # examples for using encoder and decoder in object detection
+    image_base64_encoded = model.base64_encoder(image_URL)
+    image_base64_decoded = model.base64_decoder(image_base64_encoded)
+    labels_returned_base64 = model.object_extraction_base64(image_base64_encoded)
+
+
 
 
 
