f5e4674a6081cf125ae8b9b46bf074a46997cfe8
Maria Sasinouskaya <maria.sasinouskaya@gmail.com>
2018-10-02 19:47:59 -0700

    --Chages for Heroku

--- src/main/java/com/recycle/buddy/Application.java
+++ src/main/java/com/recycle/buddy/Application.java
@@ -2,9 +2,16 @@ package com.recycle.buddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class Application {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
