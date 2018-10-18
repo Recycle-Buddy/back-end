14ad09e626160beacdd575da539a6fb91ffe05de
Maria <30535696+MariaSS88@users.noreply.github.com>
2018-10-18 12:09:55 -0700

    Recycle buddy java maria (#13)
    * --Added properties
    
    * --added plugin for maven + spring boot
    
    * --added test response for front end
    
    * --put response from AutoML model to the response to front end
    --changed pom for deploying jar on heroku
    
    * --added matching response from ML model to labels (for demo presentation only)
    
    * --added default statement for switch
    
    * --added default statement for switch
    --cleaned from commented code
    
    * --added response model

---
+++ src/main/java/com/recycle/buddy/model/output/Error.java
@@ -0,0 +1,13 @@
+package com.recycle.buddy.model.output;
+
+public class Error {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
